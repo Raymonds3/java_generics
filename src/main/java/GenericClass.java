63e48f45d648214fce566820525f71d184a38545
Raymond Serekwane <raymonds338@gmail.com>
2020-04-10 11:57:23 +0200

    first commit

---
+++ src/main/java/GenericClass.java
@@ -0,0 +1,39 @@
+public class GenericClass<T> {
+    private T type;
+
+    public void setType(T type) {
+        this.type = type;
+    }
+
+    public T getType() {
+        return type;
+    }
+
+    public GenericClass(T type) {
+        this.type = type;
+    }
+
+    public static <T> void ArrayContent(T[] type) {
+     for (T i : type) {
+         System.out.println(i);
+     }
+        System.out.println();
+    }
+
+    public static void main(String[] Args){
+        Integer[] intArray = { 12, 324, 6, 7900 };
+        String[] stringArray = { "Raymond", "Thabo", "Siya", "John", "Skhumba" };
+
+        System.out.println("Integer Array Contains:");
+        ArrayContent(intArray);
+
+        System.out.println("String Array Contains:");
+        ArrayContent(stringArray);
+
+        GenericClass<String> string = new GenericClass<String>("Test");
+        System.out.println(string.type);
+
+        GenericClass<Integer> number = new GenericClass<Integer>(1000);
+        System.out.println(number.type);
+    }
+}
